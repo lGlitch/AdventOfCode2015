0d0ec3c26698fd5eef790518aa85485122781f21
LeGlitch <erewannBareth89@gmail.com>
2025-11-12 15:06:40 +0100

    Exo1

---
+++ AdventOfCode/Exo1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Exo1
+    {
+        public static void Solution()
+        {
+            String Inupt = "";
+            int nb = 0;
+            foreach (var item in Inupt)
+            {
+                if (item == '(')
+                {
+                    nb += 1;
+                }
+                else { nb -= 1; }
+            }
+            Console.WriteLine(nb);
+        }
+    }
+}
